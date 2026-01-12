32bfe40927f8881a58d4502c92f00c7e0ce6a194
Rakib Hassan <mailrakibhassan@gmail.com>
2026-01-12 21:58:40 +0600

    error and loading file added in home group route

---
+++ src/components/shared/error-ui.tsx
@@ -0,0 +1,29 @@
+import { Card, Center } from "@chakra-ui/react";
+import { Frown } from "lucide-react";
+import StyledButton from "@/components/shared/styled-button";
+import { useEffect } from "react";
+
+export default function ErrorUI({ error }: { error: Error }) {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
+  return (
+    <Center h="full">
+      <Card.Root w="full" maxW="320px" rounded="xl">
+        <Card.Body gap="2">
+          <Frown size="30" color="gray" />
+          <Card.Title fontSize="4xl">Oops!</Card.Title>
+          {/* error message */}
+          <Card.Description>{error.message}</Card.Description>
+        </Card.Body>
+        {/* try again button to reload the page */}
+        <Card.Footer>
+          <StyledButton onClick={() => window.location.reload()}>
+            Try Again
+          </StyledButton>
+        </Card.Footer>
+      </Card.Root>
+    </Center>
+  );
+}
