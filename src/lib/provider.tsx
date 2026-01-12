6181b94bbac029284de1982af9be9586c5f358ef
Rakib Hassan <mailrakibhassan@gmail.com>
2026-01-12 14:06:36 +0600

    providers component created and called in root layout

---
+++ src/lib/provider.tsx
@@ -0,0 +1,19 @@
+import { Provider as ChakraUIProvider } from "@/components/ui/provider";
+import { Toaster } from "@/components/ui/toaster";
+import { Provider as ReduxStoreProvider } from "@/redux/provider";
+import { cookies } from "next/headers";
+
+export default async function Providers({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  // if the user is authenticated the cookie will be found
+  const token = (await cookies()).get("token")?.value;
+
+  return (
+    // don't use system preference use "light" theme by default
+    <ChakraUIProvider enableSystem={false}>
+      <ReduxStoreProvider token={token}>{children}</ReduxStoreProvider>
+      <Toaster />
+    </ChakraUIProvider>
+  );
+}
