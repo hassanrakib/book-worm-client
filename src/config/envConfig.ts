2c6c5c2cf2ac78219ce38deb587bd2b56dc0d2c7
Rakib Hassan <mailrakibhassan@gmail.com>
2026-01-12 13:45:19 +0600

    config files added

---
+++ src/config/envConfig.ts
@@ -0,0 +1,5 @@
+export const envConfig = {
+  NODE_ENV: process.env.NODE_ENV,
+  token_expires_in: process.env.TOKEN_EXPIRES_IN,
+  baseApi: process.env.NEXT_PUBLIC_BASE_API,
+};
---
+++ src/config/fonts.ts
@@ -0,0 +1,7 @@
+import { Sora } from "next/font/google";
+
+// If loading a variable font, you don't need to specify the font weight
+export const sora = Sora({
+  subsets: ["latin"],
+  display: "swap",
+});
